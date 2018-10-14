a0c248f78d13cbdae457c4574c00bc1a8d309c7a
Igor_Buryanenko <buri_itrp@hotmail.com>
2018-10-14 13:44:25 +0300

    Record of the visitor, check of possibility - 2.

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 exports.router = express.Router();
-var parser = require('body-parser');
-var userModel = require('../models/user')
+// var parser = require('body-parser');
+// var userModel = require('../models/user')
 
 var mysql = require('mysql');
 
@@ -35,12 +35,12 @@ var DAYS_FOR_SEARCH = 7;
 var DEAD_ZONE = 120;
 
 /* GET home page. */
-exports.router.get('/', function (req, res, next) {
+exports.router.get('/', function (req, res) {
     res.render('index', {title: 'Express'});
 });
 
 
-exports.router.get('/:userName', function (req, res, next) {
+exports.router.get('/:userName', function (req, res) {
     var usr = req.params.userName;
     var patterns = [];
     // con.query('select distinct type, description from eventpattern ' +
@@ -48,7 +48,7 @@ exports.router.get('/:userName', function (req, res, next) {
         'left join users on eventpattern.userId = users.userId ' +
         'left join eventslist on eventpattern.patternId = eventslist.patternId and eventslist.date >= curdate() ' +
         'where users.login = ? group by type, duration, number;',
-        usr, function (err, results, fields) {
+        usr, function (err, results) {
             if (err) throw err;
             results.forEach(function (entry) {
                 if (entry.patternId != null)
@@ -59,10 +59,10 @@ exports.router.get('/:userName', function (req, res, next) {
         });
 });
 
-exports.router.get('/:userName/:patternId', function (req, res, next) {
+exports.router.get('/:userName/:patternId', function (req, res) {
     var usr = req.params.userName;
     var patternId = req.params.patternId;
-    con.query('select userId from users where login = ?', usr, function (err, results, fields) {
+    con.query('select userId from users where login = ?', usr, function (err, results) {
         if (err) throw err;
         if (results.length == 0) res.render('index', {description: '', duration: 0, dates: []});
         else {
@@ -72,11 +72,11 @@ exports.router.get('/:userName/:patternId', function (req, res, next) {
                 'left join eventvisitors v on v.eventId = e.eventId  ' +
                 'where p.patternId = ? and e.date >= curdate() and (datediff(e.date, curdate()) <= ?) ' +
                 'group by e.eventId order by e.date;',
-                conditions, function (err, rslts, fields) {
+                conditions, function (err, rslts) {
                     if (err) throw err;
                     var dayCur = new Date();
                     var days = weekAvailable(dayCur, rslts);
-                    res.render('timepicker', {userName: usr, eventType: rslts[0].type, description: rslts[0].description,
+                    res.render('timepicker', {username: usr, eventType: rslts[0].type, description: rslts[0].description,
                         duration: rslts[0].duration, patternId: patternId, days: days});
                 });
         }
@@ -84,27 +84,27 @@ exports.router.get('/:userName/:patternId', function (req, res, next) {
 });
 
 // modelling post-queries as get-queries
-exports.router.get('/getWeek/:date/:patternId', function (req, res, next) {
+exports.router.get('/getWeek/:date/:patternId', function (req, res) {
     var conditions = [req.params.patternId, req.params.date, req.params.date, DAYS_FOR_SEARCH];
     con.query('select e.date, e.time, p.number, count(v.visitorId) as amount from eventslist e ' +
         'left join eventpattern p on p.patternId = e.patternId   ' +
         'left join eventvisitors v on v.eventId = e.eventId where e.patternId = ? and e.date >= ? and (datediff(e.date, ?) < ?) ' +
         'group by e.eventId;',
-        conditions, function (err, results, fields) {
+        conditions, function (err, results) {
             if (err) throw err;
             var days = weekAvailable(new Date(req.params.date), results);
             res.json({days: days});
         });
 });
 
-exports.router.get('/getTimeLine/:date/:patternId', function (req, res, next) {
+exports.router.get('/getTimeLine/:date/:patternId', function (req, res) {
     var conditions = [req.params.patternId, req.params.date];
-    console.log('hi')
+    // console.log('hi');
     con.query(`select e.date, e.time, e.eventId, p.number, count(v.visitorId) as amount from eventslist e 
     right join eventpattern p on p.patternId = e.patternId  
     left join eventvisitors v on v.eventId = e.eventId
     where e.patternId = ? and e.date = ? group by e.eventId order by e.time;` ,
-        conditions, function (err, results, fields) {
+        conditions, function (err, results) {
             if (err) throw err;
             console.log(results);
             var events = [];
@@ -118,25 +118,25 @@ exports.router.get('/getTimeLine/:date/:patternId', function (req, res, next) {
                 events.push({date: entry.date, time: entry.time, eventId: entry.eventId, remain: (entry.number - entry.amount),
                     availability: (deadZoneBoundary < dateTime) && (entry.number - entry.amount > 0)});
             });
-            console.log(events)
+            console.log(events);
             res.json({events: events});
         });
 });
 
-exports.router.post('/submitVisitor', function (req, res, next) {
+exports.router.post('/submitVisitor', function (req, res) {
     let vname = req.body.name;
     let vemail = req.body.email;
     let eventId = req.body.event;
     // console.log(eventId);
 
     let visitor = [vname, vemail, vname, vemail];
-    let vstrId, eventCapacity, access;
+    let vstrId, eventCapacity, eventPattern, access;
 
     //  checking the possibility of recording on the event
-    con.query('select e.date, e.time, p.number, p.multiaccess, count(v.visitorId) as amount from eventslist e ' +
+    con.query('select e.date, e.time, p.number, p.multiaccess, p.patternId, count(v.visitorId) as amount from eventslist e ' +
         'left join eventpattern p on p.patternId = e.patternId ' +
         'left join eventvisitors v on v.eventId = e.eventId where e.eventId = ? ;',
-        [eventId], function (err, possibility, fields) {
+        [eventId], function (err, possibility) {
             if (err) throw err;
             if (possibility.length == 0 || possibility[0].number - possibility[0].amount < 1) {   // recording possibility  = false
                 console.log('Sorry...');
@@ -145,9 +145,10 @@ exports.router.post('/submitVisitor', function (req, res, next) {
             else {                                                                                // recording possibility  = true
                 //   input visitor ( + uniqueness check)
                 eventCapacity = possibility[0].number;
+                eventPattern = possibility[0].patternId;
                 access = possibility[0].multiaccess;
                 con.query('select visitorId from visitors where name = ? and email = ?;',
-                    visitor, function (err, visitorid, fields) {
+                    visitor, function (err, visitorid) {
                         if (err) throw err;
                         if (visitorid.length != 0) {
                             vstrId = visitorid[0].visitorId;
@@ -155,7 +156,7 @@ exports.router.post('/submitVisitor', function (req, res, next) {
                         else {
                             con.query('insert into visitors (name, email) select * from (select ?, ?) as tmp ' +
                                 'where not exists(select * from visitors where name = ? and email = ?) limit 1;',
-                                visitor, function (err, results, fields) {
+                                visitor, function (err, results) {
                                     if (err) throw err;
                                     vstrId = results.insertId;
                                 });
@@ -163,62 +164,70 @@ exports.router.post('/submitVisitor', function (req, res, next) {
 
                         // checking for duplicate entries on an event
                         con.query(`select count(visitorId) as amount from eventvisitors
-                        where eventId = ? and visitorId = ?`, [eventId, vstrId], function (err, duplicate, fields) {
+                        where eventId = ? and visitorId = ?`, [eventId, vstrId], function (err, duplicate) {
                             if (err) throw err;
                             if (duplicate[0].amount == 0) {
 
                                 // checking for pattern restrictions
-                                if (access > 0) {
-                                    con.query(`select count(v.evId) as alreadyRecords from eventvisitors v 
-                                      left join eventslist e on v.eventId = e.eventId 
-                                      left join eventpattern p on p.patternId = e.patternId
-                                      where v.visitorId = ?;`, [vstrId], function (err, isRecords, fields) {
+                                if (access > 0) {  // there are restrictions on the number of pattern events that a visitor can be recorded
+                                    con.query('select count(v.evId) as alreadyRecords from eventvisitors v ' +
+                                        'where v.visitorId = ? and v.eventId in ' +
+                                        '(select e.eventId from eventslist e where e.patternId = ?);',
+                                        [vstrId, eventPattern], function (err, isRecords) {
                                         if (err) throw err;
-
+                                        if (isRecords[0].alreadyRecords < access) {  // it's still possible to record to the pattern events
+                                            eventRecording(eventId, vstrId, eventCapacity);
+                                            res.json({success: 0, name: vname, email: vemail});
+                                        }
+                                        else {  // it's impossible to be written to the pattern events, because the visitor has already been recorded to other pattern events
+                                            res.json({success: 3, name: vname, email: vemail});
+                                        }
                                     });
                                 }
-
-
-                                //  event recording
-                                con.query('insert into eventvisitors (eventId, visitorId) values (?, ?);',
-                                    [eventId, vstrId], function (err, recording, fields) {
-                                        if (err) throw err;
-                                        //  collision check
-                                        con.query(`select count(visitorId) as amount 
-                                            from eventvisitors where eventId = ?`,
-                                            [eventId], function (err, completeness, fields) {
-                                                if (err) throw err;
-                                                if (completeness[0].amount > eventCapacity) {  // too many visitors've recorded to the event
-                                                    con.query(`select evId, visitorId from eventvisitors where eventId = ? order by evId desc limit 1 ;`,
-                                                        [eventId], function (err, lastVisitor, fields) {
-                                                            if (lastVisitor[0].visitorId == vstrId) {   // our visitor is last one who's recorded to the event
-                                                                con.query(`delete from eventvisitors where evId = ?`,
-                                                                    [lastVisitor[0].evId], function (err, delVisitEv, fields) {
-                                                                        if (err) throw err;
-                                                                        if (delVisitEv.affectedRows == 1) {
-                                                                            console.log('Removed excess');
-                                                                        }
-                                                                    })
-                                                            }
-                                                        })
-                                                }
-                                                else {
-                                                    console.log('Success!');
-                                                    res.json({success: 0, name: vname, email: vemail});
-                                                }
-                                            });
-                                    });
+                                else {  // there are no restrictions on the number of pattern events that a visitor can be recorded
+                                    eventRecording(eventId, vstrId, eventCapacity);
+                                    res.json({success: 0, name: vname, email: vemail});
+                                }
                             }
-                            else {   //  visitor has already recorded on this event
+                            else {   //  visitor has already recorded on this event earlier
                                 console.log("You've already recorded on this event.");
                                 res.json({success: 2, name: vname, email: vemail});
                             }
                         });
                     });
             }
-            // console.log('End of checking.');
         });
 });
+
+function eventRecording(eventId, vstrId, eventCapacity) {
+    //  event recording
+    con.query('insert into eventvisitors (eventId, visitorId) values (?, ?);',
+        [eventId, vstrId], function (err, recording) {
+            if (err) throw err;
+            //  collision check
+            con.query('select count(visitorId) as amount from eventvisitors where eventId = ?;',
+                [eventId], function (err, completeness) {
+                    if (err) throw err;
+                    if (completeness[0].amount > eventCapacity) {  // too many visitors've recorded to the event
+                        con.query('select evId, visitorId from eventvisitors where eventId = ? order by evId desc limit 1 ;',
+                            [eventId], function (err, lastVisitor) {
+                                if (lastVisitor[0].visitorId == vstrId) {   // our visitor is last one who's recorded to the event
+                                    con.query('delete from eventvisitors where evId = ?',
+                                        [lastVisitor[0].evId], function (err, delVisitEv) {
+                                            if (err) throw err;
+                                            if (delVisitEv.affectedRows == 1) {
+                                                console.log('Removed excess');
+                                            }
+                                        })
+                                }
+                            })
+                    }
+                    else {
+                        console.log('Success!');
+                    }
+                });
+        });
+}
 
 function weekAvailable (dayCur, rsl) {
     var days = [];
